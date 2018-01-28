3f497feeb687eb619c7024bc8aa87e830986cfcf
Sandeep Ghimire <sandeepghimire@Sandeeps-MacBook-Air.local>
2018-01-28 21:22:49 +0100

    Html renderer edit

--- Node Html renderer/server.js
+++ Node Html renderer/server.js
@@ -1,28 +1,20 @@
 var http = require('http');
-var module1 = require('./module1');
-var module2 = require('./module2');
 var fs = require('fs');
 
 function onRequest(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write(module2.myString);
-  module2.myFunction(); /* here we need the parenthesis because we are ex
-  ecuting it */
-  fs.readFile('./index.html', null , function(error, data) {
-    if(error) {
-      response.writeHead(404);
-      response.write('file not found');
-    }
-    else {
-      response.writeHead(data);
-    }
-    response.end();
-  });
-  
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', null, function(error, data) {
+        if (error) {
+            response.writeHead(404);
+            response.write('File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
 }
 
 http.createServer(onRequest).listen(8000);
-
 
 
 
